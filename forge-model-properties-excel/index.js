e373a2e7e10c33fe219cfc370283bc547f30d9cb
xiaodongliang <xiaodongliang@hotmail.com>
2021-07-04 14:47:20 +0800

    fix some logics

--- forge-model-properties-excel/index.js
+++ forge-model-properties-excel/index.js
@@ -4,16 +4,14 @@ const ExcelJS = require('exceljs');
 const { AuthClientTwoLegged, DerivativesApi } = require('forge-apis');
 const config = require('./config');
 
-let forge_auth = new AuthClientTwoLegged(config.client_id, config.client_secret, config.scopes);
-let forge_deriv = new DerivativesApi();
-
-
-const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_MODEL_URN } = process.env;
-if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET || !FORGE_MODEL_URN) {
+if (!config.client_id || !config.client_secret || !config.model_urn) {
     console.error('Some of the following env. variables are missing:');
     console.error('FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, MODEL_URN');
     return;
 }
+
+let forge_auth = new AuthClientTwoLegged(config.client_id, config.client_secret, config.scopes);
+let forge_deriv = new DerivativesApi();
 
 (async () => {
     try {
@@ -28,75 +26,56 @@ if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET || !FORGE_MODEL_URN) {
             if (meta3DviewUrn) {
 
                 //follow the same format of GET:Properties to build the response
-                let propertiesRes = { body: { result: 'success' } }
+                let propertiesRes = { statusCode: 202 }
 
-                //set a timeout to avoid long time waiting. run the script after some time.
+                //set a timeout to avoid long time waiting. 
+                //If timeout, run the scripts again to check if it completes.
+
+                //5 minutes is NOT a standard. adjust it if needed 
+                //(note: token will be expire in 60 minutes since it is generated. 
+                //may need to re-generate token if timeout is too long.
+                const timeOut = 5 * 60 * 1000   //5 minutes
+                //to avoid 429 (rate limit), set interval of the each call
+                //60 calls per minute for force getting large resource
+                const rateInterval = 10 * 1000  //10 seconds
+
+                //start time
                 const st = new Date().getTime()
-
                 //waiting for the extracting.   
-                while (propertiesRes.body.result == 'success' &&
-                    new Date().getTime() - st < 5 * 60 * 1000) { // 5 minutes  timeout 
+                console.log(`extracting properties......timeout ${timeOut/60/1000} minutes `);
+
+                while (propertiesRes.statusCode == 202
+                    &&
+                    new Date().getTime() - st < timeOut) {
+
                     propertiesRes.result == null
-                    propertiesRes = await forge_deriv.getModelviewProperties(config.model_urn, meta3DviewUrn.guid, { forceget: true }, forge_auth, forge_auth.getCredentials());
+
+                    //wait some seconds to perform the next call
+                    await delay(rateInterval)
+
+                    propertiesRes = await forge_deriv.getModelviewProperties(config.model_urn,
+                        meta3DviewUrn.guid,
+                        { forceget: true },
+                        forge_auth,
+                        forge_auth.getCredentials());
                 }
 
-                if (propertiesRes.body.result) {
-                    //the extracting has not been completed. The While loop breaks due to time out
-                    console.log('the extracting has not been completed. The While loop breaks due to time out. Try to run the script again after some time');
 
-                } else {
-
+                if (propertiesRes.statusCode == 200) {
+                    //successful extraction, writing data to excel
                     const allProperties = propertiesRes.body.data.collection
-
-                    //prepare rows. get dbid and material only
-                    let rows = [];
-                    allProperties.forEach(async p => { 
-                        try{ 
-                            let hasMaterial = p.properties['Materials and Finishes']
-
-                            if (hasMaterial && hasMaterial['Material'] != null) {
-                                let eachrow = [p.objectid+'', hasMaterial['Material']]
-                                rows.push(eachrow);
-                            }
-                        }catch(e){}
-                    });
-
-
-                    //create workbook
-                    const workbook = new ExcelJS.Workbook();
-                    var worksheet = workbook.addWorksheet('powerbi_forge')
-
-                    //define columns
-
-                    const columns = [
-                        { name: 'dbid', filterButton: false },
-                        { name: 'material', filterButton: false },
-                    ]
-
-                    //create table with the columns and rows
-                    worksheet.addTable({
-                        name: 'powerbi_forge',
-                        ref: 'A1',
-                        headerRow: true,
-                        totalsRow: true,
-                        style: {
-                            theme: 'TableStyleDark3',
-                            showRowStripes: true,
-                        },
-                        columns: columns,
-                        rows: rows
-                    });
-
-                    //save the excel file
-                    const buffer = await workbook.xlsx.writeBuffer();
-                    fs.writeFile("powerbi_forge.xlsx", buffer, "binary", function (err) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log("The Excel file was saved!");
-                        }
-                    })
+                    await produceExcelFile(allProperties) 
+                }
+                else if (propertiesRes.statusCode == 202 ) {
+                    // While loop breaks due to time out
+                    console.log(`The While loop breaks due to time out ${timeOut/60/1000} minutes. Try to run the script again after some time`);
                 } 
+                else if (propertiesRes.statusCode == 429 ) {
+                    console.log(`hit rate limit. please wait for some time to run the scripts again, and increase rateInterval. Currently rateInterval=${rateInterval}/60 seconds `);
+                }
+                else { 
+                    console.log(`other unknown errors. debug to check what the problem is.`);
+                }
 
             } else {
                 console.error('this model has no {3D} view')
@@ -110,3 +89,63 @@ if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET || !FORGE_MODEL_URN) {
         console.error(e);
     }
 })()
+
+
+//write properties data to excel file
+async function produceExcelFile(allProperties) {
+    //prepare rows. get dbid and material only
+    let rows = [];
+    allProperties.forEach(async p => {
+        try {
+            let hasMaterial = p.properties['Materials and Finishes']
+
+            if (hasMaterial && hasMaterial['Material'] != null) {
+                let eachrow = [p.objectid + '', hasMaterial['Material']]
+                rows.push(eachrow);
+            }
+        } catch (e) { }
+    });
+
+
+    //create workbook
+    const workbook = new ExcelJS.Workbook();
+    var worksheet = workbook.addWorksheet('powerbi_forge')
+
+    //define columns
+
+    const columns = [
+        { name: 'dbid', filterButton: false },
+        { name: 'material', filterButton: false },
+    ]
+
+    //create table with the columns and rows
+    worksheet.addTable({
+        name: 'powerbi_forge',
+        ref: 'A1',
+        headerRow: true,
+        totalsRow: true,
+        style: {
+            theme: 'TableStyleDark3',
+            showRowStripes: true,
+        },
+        columns: columns,
+        rows: rows
+    });
+
+    //save the excel file
+    const buffer = await workbook.xlsx.writeBuffer();
+    fs.writeFile("powerbi_forge.xlsx", buffer, "binary", function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("The Excel file was saved!");
+        }
+    })
+}
+
+//to avoid the problem of 429 (too many requests in a time frame)
+async function delay(t, v) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve.bind(null, v), t);
+    });
+}
