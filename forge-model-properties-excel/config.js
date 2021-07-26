41b0466462bf4177f817960f9c6d6d942e278e26
xiaodongliang <xiaodongliang@hotmail.com>
2021-07-26 10:03:52 +0800

    Update config.js

--- forge-model-properties-excel/config.js
+++ forge-model-properties-excel/config.js
@@ -1,11 +1,11 @@
 
 module.exports = { 
    
-    client_id: process.env.FORGE_CLIENT_ID ,
-    client_secret: process.env.FORGE_CLIENT_SECRET,
+    client_id: process.env.FORGE_CLIENT_ID||'<your client id of Forge>' ,
+    client_secret: process.env.FORGE_CLIENT_SECRET || '<your client secret of Forge>',
      
-    model_urn: process.env.FORGE_MODEL_URN,
-    bucket: process.env.FORGE_BUCKET_KEY,
+    model_urn: process.env.FORGE_MODEL_URN || '<your model urn of Forge>',
+    //bucket: process.env.FORGE_BUCKET_KEY || '<your bucket of Forge>',
 
     scopes: ['data:read'],
     token:''
